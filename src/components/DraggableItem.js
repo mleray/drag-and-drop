982adc299f3415927f1ad62e0ce48bc65a222c1b
mleray <maud.leray035@gmail.com>
2019-04-19 12:57:21 +0200

    Created DraggableItem component

---
+++ src/components/DraggableItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { object, string } from 'prop-types';
+import injectSheet from 'react-jss';
+
+// Helpers
+import dnd from '../helpers/dragAndDrop';
+
+const styles = {
+	container: {
+		border: 'thin solid black',
+		borderRadius: 4,
+		padding: '0 30px',
+		backgroundColor: 'lightgrey',
+		textAlign: 'center',
+		flex: 1,
+		margin: '0 10px',
+		cursor: 'pointer',
+		'&:hover': {
+			backgroundColor: 'lightblue'
+		}
+	}
+};
+
+const DraggableItem = ({ classes, name }) => (
+	<div
+		id={name.toLowerCase()}
+		className={classes.container}
+		draggable="true"
+		onDragStart={event => dnd.drag(event)}
+	>
+		<p> {name} </p>
+	</div>
+);
+
+DraggableItem.propTypes = {
+	classes: object.isRequired,
+	name: string.isRequired
+};
+
+export default injectSheet(styles)(DraggableItem);
